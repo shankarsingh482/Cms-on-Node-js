ac2e6a30f1d2ef2b73c6ec2ce3101ca94db0288e
Shankar Singh <shankarsingh.ss482@gmail.com>
2018-09-07 12:28:18 +0530

    Update prod-database.js

--- config/prod-database.js
+++ config/prod-database.js
@@ -2,7 +2,7 @@ module.exports = {
 
 
 
-    mongoDbUrl : 'mongodb://shankar123:shanky@2021@ds161950.mlab.com:61950/cmsdata'
+    mongoDbUrl : 'mongodb://shankar12:shanky2021@ds161950.mlab.com:61950/cmsdata'
 
     /*
     mongoDbUrl: process.env.MONGO_DB_URI
